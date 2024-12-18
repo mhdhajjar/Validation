b62e0f2a4fa2eeba6f51276907260f87687adf85
tamara_el_hajjar <tamara.el_hajjar@ensta-bretagne.org>
2024-12-18 11:51:59 +0100

    second commit

--- bfsTraversal.py
+++ bfsTraversal.py
@@ -2,7 +2,7 @@ from collections import deque
 
 def bfsTraversal(graph, pred, opaque):
     i = True 
-    k = set()  
+    k = set([])  # Use set([]) for compatibility with Python 2
     F = deque()  
 
     while len(F) > 0 or i:
@@ -25,33 +25,52 @@ def bfsTraversal(graph, pred, opaque):
 
 
 class SimpleGraph:
-    def __init__(self, graph_dict, roots):
-        self.graph_dict = graph_dict  
-        self.roots = roots  
+    def __init__(self, graph_dict, roots):  # Corrected constructor
+        self.graph_dict = graph_dict  # Dictionary representing the graph
+        self.roots = roots  # List of root nodes
 
     def neighbours(self, S):
         return self.graph_dict.get(S, [])
 
 
+class NBits:
+    def __init__(self, roots, N):  # Corrected constructor
+        self.roots = roots  # List of roots
+        self.N = N          # Size of the graph
+
+    def neighbours(self, S):
+        # Returns the neighbors of node S based on bit representation
+        neighbours = []
+        for i in range(self.N):  # Use xrange for Python 2
+            if S & (1 << i):  # If the ith bit is set, then i is a neighbor of S
+                neighbours.append(i)
+        return neighbours
+
+
 def pred(n, opaque):
     target = opaque.get("target")
     if n == target:
-        return True 
+        return True  # Terminate as soon as we find the target
     return False
 
 
-graphe_exemple = {
+# Example with a graph represented by a dictionary
+graph_dict = {
     1: [3, 2],
     2: [1, 3, 4],
     3: [3],
     4: []
 }
-
-roots = [1] 
-
+roots = [1]
 opaque = {"target": 2}
+graph_dict_example = SimpleGraph(graph_dict, roots)
+result = bfsTraversal(graph_dict_example, pred, opaque)
+print(result)  # Use print statement (no parentheses)
 
-graph = SimpleGraph(graphe_exemple, roots)
-
-result = bfsTraversal(graph, pred, opaque)
-print(result)
+# Example with a graph represented by bits
+N = 5  # For example, a graph with 5 nodes
+roots = [0]  # Start from node 0
+opaque = {"target": 3}
+graph_bits_example = NBits(roots, N)
+result = bfsTraversal(graph_bits_example, pred, opaque)
+print(result) # Use print statement (no parentheses)
